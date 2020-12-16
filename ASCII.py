678afd03942ec7a6e201d81e261177f5c9a850e6
oooookeyyyyy <73366852+oooookeyyyyy@users.noreply.github.com>
2020-12-16 05:25:25 -0800

    Add files via upload

---
+++ ASCII.py
@@ -0,0 +1,779 @@
+from os import system
+from colorama import Fore
+#SupporteD ChracterS :
+a ='''
+ █████╗ 
+██╔══██╗
+███████║
+██╔══██║
+██║  ██║
+╚═╝  ╚═╝
+'''.split('\n')
+b = '''
+██████╗ 
+██╔══██╗
+██████╔╝
+██╔══██╗
+██████╔╝
+╚═════╝ 
+'''.split('\n')
+c = '''
+ ██████╗
+██╔════╝
+██║     
+██║     
+╚██████╗
+ ╚═════╝
+'''.split('\n')
+d = '''
+██████╗ 
+██╔══██╗
+██║  ██║
+██║  ██║
+██████╔╝
+╚═════╝ 
+'''.split('\n')
+e = '''
+███████╗
+██╔════╝
+█████╗  
+██╔══╝  
+███████╗
+╚══════╝
+'''.split('\n')
+f = '''
+███████╗
+██╔════╝
+█████╗  
+██╔══╝  
+██║     
+╚═╝     
+'''.split('\n')
+g = '''
+ ██████╗ 
+██╔════╝ 
+██║  ███╗
+██║   ██║
+╚██████╔╝
+ ╚═════╝ 
+'''.split('\n')
+h = '''
+██╗  ██╗
+██║  ██║
+███████║
+██╔══██║
+██║  ██║
+╚═╝  ╚═╝
+'''.split('\n')
+i = '''
+██╗
+██║
+██║
+██║
+██║
+╚═╝
+'''.split('\n')
+j = '''
+     ██╗
+     ██║
+     ██║
+██   ██║
+╚█████╔╝
+ ╚════╝ 
+'''.split('\n')
+k = '''
+██╗  ██╗
+██║ ██╔╝
+█████╔╝ 
+██╔═██╗ 
+██║  ██╗
+╚═╝  ╚═╝
+'''.split('\n')
+l = '''
+██╗     
+██║     
+██║     
+██║     
+███████╗
+╚══════╝
+'''.split('\n')
+m = '''
+███╗   ███╗
+████╗ ████║
+██╔████╔██║
+██║╚██╔╝██║
+██║ ╚═╝ ██║
+╚═╝     ╚═╝
+'''.split('\n')
+n = '''
+███╗   ██╗
+████╗  ██║
+██╔██╗ ██║
+██║╚██╗██║
+██║ ╚████║
+╚═╝  ╚═══╝
+'''.split('\n')
+o = '''
+ ██████╗ 
+██╔═══██╗
+██║   ██║
+██║   ██║
+╚██████╔╝
+ ╚═════╝ 
+'''.split('\n')
+p = '''
+██████╗ 
+██╔══██╗
+██████╔╝
+██╔═══╝ 
+██║     
+╚═╝     
+'''.split('\n')
+q = '''
+ ██████╗ 
+██╔═══██╗
+██║   ██║
+██║▄▄ ██║
+╚██████╔╝
+ ╚══▀▀═╝ 
+'''.split('\n')
+r = '''
+██████╗ 
+██╔══██╗
+██████╔╝
+██╔══██╗
+██║  ██║
+╚═╝  ╚═╝
+'''.split('\n')
+s = '''
+███████╗
+██╔════╝
+███████╗
+╚════██║
+███████║
+╚══════╝
+'''.split('\n')
+t = '''
+████████╗
+╚══██╔══╝
+   ██║   
+   ██║   
+   ██║   
+   ╚═╝   
+'''.split('\n')
+u = '''
+██╗   ██╗
+██║   ██║
+██║   ██║
+██║   ██║
+╚██████╔╝
+ ╚═════╝ 
+'''.split('\n')
+v = '''
+██╗   ██╗
+██║   ██║
+██║   ██║
+╚██╗ ██╔╝
+ ╚████╔╝ 
+  ╚═══╝  
+'''.split('\n')
+w = '''
+██╗    ██╗
+██║    ██║
+██║ █╗ ██║
+██║███╗██║
+╚███╔███╔╝
+ ╚══╝╚══╝ 
+'''.split('\n')
+x = '''
+██╗  ██╗
+╚██╗██╔╝
+ ╚███╔╝ 
+ ██╔██╗ 
+██╔╝ ██╗
+╚═╝  ╚═╝
+'''.split('\n')
+y = '''
+██╗   ██╗
+╚██╗ ██╔╝
+ ╚████╔╝ 
+  ╚██╔╝  
+   ██║   
+   ╚═╝   
+'''.split('\n')
+z = '''
+███████╗
+╚══███╔╝
+  ███╔╝ 
+ ███╔╝  
+███████╗
+╚══════╝
+'''.split('\n')
+one = '''
+ ██╗
+███║
+╚██║
+ ██║
+ ██║
+ ╚═╝
+'''.split('\n')
+two = '''
+██████╗ 
+╚════██╗
+ █████╔╝
+██╔═══╝ 
+███████╗
+╚══════╝
+'''.split('\n')
+three = '''
+██████╗ 
+╚════██╗
+ █████╔╝
+ ╚═══██╗
+██████╔╝
+╚═════╝ 
+'''.split('\n')
+four = '''
+██╗  ██╗
+██║  ██║
+███████║
+╚════██║
+     ██║
+     ╚═╝
+'''.split('\n')
+five = '''
+███████╗
+██╔════╝
+███████╗
+╚════██║
+███████║
+╚══════╝
+'''.split('\n')
+six = '''
+ ██████╗ 
+██╔════╝ 
+███████╗ 
+██╔═══██╗
+╚██████╔╝
+ ╚═════╝ 
+'''.split('\n')
+seven = '''
+███████╗
+╚════██║
+    ██╔╝
+   ██╔╝ 
+   ██║  
+   ╚═╝  
+'''.split('\n')
+eight = '''
+ █████╗ 
+██╔══██╗
+╚█████╔╝
+██╔══██╗
+╚█████╔╝
+ ╚════╝ 
+'''.split('\n')
+nine = '''
+ █████╗ 
+██╔══██╗
+╚██████║
+ ╚═══██║
+ █████╔╝
+ ╚════╝ 
+'''.split('\n')
+zero = '''
+ ██████╗ 
+██╔═████╗
+██║██╔██║
+████╔╝██║
+╚██████╔╝
+ ╚═════╝ 
+'''.split('\n')
+underline = '''
+         
+         
+         
+         
+████████╗
+╚═══════╝
+'''.split('\n')
+adsign = '''
+ ██████╗ 
+██╔═══██╗
+██║██╗██║
+██║██║██║
+╚█║████╔╝
+ ╚╝╚═══╝ 
+'''.split('\n')
+hashtag = '''
+ ██╗ ██╗ 
+████████╗
+╚██╔═██╔╝
+████████╗
+╚██╔═██╔╝
+ ╚═╝ ╚═╝ 
+'''.split('\n')
+wtf = '''
+██╗
+██║
+██║
+╚═╝
+██╗
+╚═╝
+'''.split('\n')
+what = '''
+██████╗ 
+╚════██╗
+  ▄███╔╝
+  ▀▀══╝ 
+  ██╗   
+  ╚═╝   
+'''.split('\n')
+slash = '''
+    ██╗
+   ██╔╝
+  ██╔╝ 
+ ██╔╝  
+██╔╝   
+╚═╝    
+'''.split('\n')
+backslash = '''
+██╗    
+╚██╗   
+ ╚██╗  
+  ╚██╗ 
+   ╚██╗
+    ╚═╝
+'''.split('\n')
+parantezbaz = '''
+ ██╗
+██╔╝
+██║ 
+██║ 
+╚██╗
+ ╚═╝
+'''.split('\n')
+parantezbas = '''
+██╗ 
+╚██╗
+ ██║
+ ██║
+██╔╝
+╚═╝ 
+'''.split('\n')
+And = '''
+   ██╗   
+   ██║   
+████████╗
+██╔═██╔═╝
+██████║  
+╚═════╝  
+'''.split('\n')
+star = '''
+       
+▄ ██╗▄ 
+ ████╗ 
+▀╚██╔▀ 
+  ╚═╝  
+       
+'''.split('\n')
+semicalen = '''
+   
+██╗
+╚═╝
+██╗
+▀╚╝
+   
+'''.split('\n')
+calen = '''
+   
+██╗
+╚═╝
+██╗
+╚═╝
+   
+'''.split('\n')
+percent = '''
+██╗ ██╗
+╚═╝██╔╝
+  ██╔╝ 
+ ██╔╝  
+██╔╝██╗
+╚═╝ ╚═╝
+'''.split('\n')
+doller = '''
+▄▄███▄▄╗
+██╔█═══╝
+███████╗
+╚══█═██║
+███████║
+╚═▀▀▀══╝
+'''.split('\n')
+minas = '''
+       
+       
+██████╗
+╚═════╝
+       
+       
+'''.split('\n')
+plus = '''
+        
+  ███╗  
+███████╗
+╚═███╔═╝
+  ╚══╝  
+        
+'''.split('\n')
+equally = '''
+        
+███████╗
+╚══════╝
+███████╗
+╚══════╝
+        
+'''.split('\n')
+kama = '''
+   
+   
+   
+   
+▄█╗
+╚═╝
+'''.split('\n')
+dot = '''
+   
+   
+   
+   
+██╗
+╚═╝
+'''.split('\n')
+smaller = '''
+    ██╗
+  ██╔═╝
+██╔═╝  
+╚═██╗  
+  ╚═██╗
+    ╚═╝
+'''.split('\n')
+bigger = '''
+██╗    
+╚═██╗  
+  ╚═██╗
+  ██╔═╝
+██╔═╝  
+╚═╝    
+'''.split('\n')
+kot = '''
+  
+█╗
+╚╝
+  
+  
+  
+'''.split('\n')
+dublekot = '''
+    
+█╗█╗
+╚╝╚╝
+    
+    
+    
+    
+'''.split('\n')
+sikh = '''
+ ███╗ 
+██╔██╗
+╚═╝╚═╝
+      
+      
+      
+'''.split('\n')
+space = '''
+    
+    
+    
+    
+    
+    
+'''.split('\n')
+while True :
+    output = ''
+    unsupported = 0
+    if system('cls') :
+        system('clear')
+
+    inp = input(Fore.BLUE+' WritE ThE TexT <PresS Enter TO ExiT> ==>'+Fore.WHITE).lower()
+    if inp == '' :
+        if system('cls') :
+            system('clear')
+
+        print('\n\t\t\t\t\t\t\t'+Fore.MAGENTA+'# WriteD BY ME!\n\n\t\t\t\t\t\t\t'+Fore.YELLOW+'TeL: @Amo_codzan')
+        input()
+        exit()
+
+    for line in range(1,8) :
+        output += '\n'
+        done = False
+        for column in range(len(inp)):
+
+            if inp[column] == 'a' :
+                output += a[line]
+                done = True
+
+            if inp[column] == 'b' :
+                output += b[line]
+                done = True
+
+            if inp[column] == 'c' :
+                output += c[line]
+                done = True
+
+            if inp[column] == 'd' :
+                output += d[line]
+                done = True
+
+            if inp[column] == 'e' :
+                output += e[line]
+                done = True
+
+            if inp[column] == 'f' :
+                output += f[line]
+                done = True
+
+            if inp[column] == 'g' :
+                output += g[line]
+                done = True
+
+            if inp[column] == 'h' :
+                output += h[line]
+                done = True
+
+            if inp[column] == 'i' :
+                output += i[line]
+                done = True
+
+            if inp[column] == 'j' :
+                output += j[line]
+                done = True
+
+            if inp[column] == 'k' :
+                output += k[line]
+                done = True
+
+            if inp[column] == 'l' :
+                output += l[line]
+                done = True
+
+            if inp[column] == 'm' :
+                output += m[line]
+                done = True
+
+            if inp[column] == 'n' :
+                output += n[line]
+                done = True
+
+            if inp[column] == 'o' :
+                output += o[line]
+                done = True
+
+            if inp[column] == 'p' :
+                output += p[line]
+                done = True
+
+            if inp[column] == 'q' :
+                output += q[line]
+                done = True
+
+            if inp[column] == 'r' :
+                output += r[line]
+                done = True
+
+            if inp[column] == 's' :
+                output += s[line]
+                done = True
+
+            if inp[column] == 't' :
+                output += t[line]
+                done = True
+
+            if inp[column] == 'u' :
+                output += u[line]
+                done = True
+
+            if inp[column] == 'v' :
+                output += v[line]
+                done = True
+
+            if inp[column] == 'w' :
+                output += w[line]
+                done = True
+
+            if inp[column] == 'x' :
+                output += x[line]
+                done = True
+
+            if inp[column] == 'y' :
+                output += y[line]
+                done = True
+
+            if inp[column] == 'z' :
+                output += z[line]
+                done = True
+
+            if inp[column] == '1' :
+                output += one[line]
+                done = True
+
+            if inp[column] == '2' :
+                output += two[line]
+                done = True
+
+            if inp[column] == '3' :
+                output += three[line]
+                done = True
+
+            if inp[column] == '4' :
+                output += four[line]
+                done = True
+
+            if inp[column] == '5' :
+                output += five[line]
+                done = True
+
+            if inp[column] == '6' :
+                output += six[line]
+                done = True
+
+            if inp[column] == '7' :
+                output += seven[line]
+                done = True
+
+            if inp[column] == '8' :
+                output += eight[line]
+                done = True
+
+            if inp[column] == '9' :
+                output += nine[line]
+                done = True
+
+            if inp[column] == '0' :
+                output += zero[line]
+                done = True
+
+            if inp[column] == '_' :
+                output += underline[line]
+                done = True
+
+            if inp[column] == '@' :
+                output += adsign[line]
+                done = True
+
+            if inp[column] == '#' :
+                output += hashtag[line]
+                done = True
+
+            if inp[column] == '!' :
+                output += wtf[line]
+                done = True
+
+            if inp[column] == '?' :
+                output += what[line]
+                done = True
+
+            if inp[column] == '/' :
+                output += slash[line]
+                done = True
+
+            if inp[column] == '\\' :
+                output += backslash[line]
+                done = True
+
+            if inp[column] == '(' :
+                output += parantezbaz[line]
+                done = True
+
+            if inp[column] == ')' :
+                output += parantezbas[line]
+                done = True
+
+            if inp[column] == '&' :
+                output += And[line]
+                done = True
+
+            if inp[column] == '*' :
+                output += star[line]
+                done = True
+
+            if inp[column] == ';' :
+                output += semicalen[line]
+                done = True
+
+            if inp[column] == ':' :
+                output += calen[line]
+                done = True
+
+            if inp[column] == '%' :
+                output += percent[line]
+                done = True
+
+            if inp[column] == '-' :
+                output += minas[line]
+                done = True
+
+            if inp[column] == '+' :
+                output += plus[line]
+                done = True
+
+            if inp[column] == '=' :
+                output += equally[line]
+                done = True
+
+            if inp[column] == ',' :
+                output += kama[line]
+                done = True
+
+            if inp[column] == '.' :
+                output += dot[line]
+                done = True
+
+            if inp[column] == '<' :
+                output += smaller[line]
+                done = True
+
+            if inp[column] == '>' :
+                output += bigger[line]
+                done = True
+
+            if inp[column] == '\'' :
+                output += kot[line]
+                done = True
+
+            if inp[column] == '\"' :
+                output += dublekot[line]
+                done = True
+
+            if inp[column] == '^' :
+                output += sikh[line]
+                done = True
+
+            if inp[column] == '$' :
+                output += doller[line]
+                done = True
+
+            if inp[column] == ' ' :
+                output += space[line]
+                done = True
+
+            if not done :
+                unsupported += 1
+
+    print(output+'\n\t'+Fore.GREEN+'with '+Fore.RED+str(unsupported)+Fore.GREEN+' UnsupporteD CharacterS')
+    unsupported = unsupported//7
+    output += '\n\twith {} UnsupporteD CharacterS\n'.format(unsupported)
+    f = open('text.txt' , 'ab')
+    f.write(bytes(output , 'utf-8'))
+    f.close()
+    print(Fore.CYAN+'\n\t\tTexT AddeD TO "text.txt"! PresS EnteR TO ContinuE\n')
+    input()
